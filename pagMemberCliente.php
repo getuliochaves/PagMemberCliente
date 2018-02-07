3f6502929ad355224cf3c577c151cca257d2aac6
Getulio <getuliochaves@gmail.com>
2018-02-07 14:55:09 -0200

    Atualização 5.9
    Atualização 5.9

--- pagMemberCliente.php
+++ pagMemberCliente.php
@@ -22,7 +22,7 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
 			'zip_url' => 'https://github.com/getuliochaves/PagMemberCliente/zipball/master', // the zip url of the GitHub repo
 			'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
 			'requires' => '3.0', // which version of WordPress does your plugin require?
-			'tested' => '4.8.2', // which version of WordPress is your plugin tested up to?
+			'tested' => '4.9.4', // which version of WordPress is your plugin tested up to?
 			'readme' => 'README.md', // which file to use as the readme for the version number
 			'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
 		);
